9d0cfb9b7bfb3994e9ff56f07ff84733eca4bdea
Alisha <anejaalisha@yahoo.com>
2017-04-30 21:12:40 +1000

    Tic tac toe board

--- lib/tic_tac_toe_gl/src/main.rs
+++ lib/tic_tac_toe_gl/src/main.rs
@@ -17,19 +17,20 @@ fn main() {
     implement_vertex!(Vertex, position);
 
     let vertex1 = Vertex { position: [0.0, 0.0] };
-    let vertex2 = Vertex { position: [1.0, 0.0] };
-    let vertex3 = Vertex { position: [1.0, 1.0] };
-    let vertex4 = Vertex { position: [0.0, 1.0] };
+    let vertex2 = Vertex { position: [0.6, 0.0] };
+    let vertex3 = Vertex { position: [0.6, 0.6] };
+    let vertex4 = Vertex { position: [0.0, 0.6] };
+    let vertex5 = Vertex { position: [0.2, 0.8] };
+    let vertex6 = Vertex { position: [0.2, -0.2] };
+    let vertex7 = Vertex { position: [0.4, 0.8] };
+    let vertex8 = Vertex { position: [0.4, -0.2] };
 
-    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+    let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6, vertex7, vertex8];
 
-    let shape_indices = vec![1u8, 0, 2, 0, 3 2];
+    let shape_indices = vec![1u8, 0, 3, 2];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::IndexBuffer::new(&display,
-                                                 glium::index::PrimitiveType::TrianglesList,
-                                                 &shape_indices)
-            .unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
 
     let vertex_shader_src = r#"
         #version 140
