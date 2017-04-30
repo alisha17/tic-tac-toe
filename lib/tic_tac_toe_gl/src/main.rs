2cf7b024a959671a2fd33e685ac19c7e273e4168
Alisha <anejaalisha@yahoo.com>
2017-04-30 11:53:28 +1000

    Triangle using index buffer glium

--- lib/tic_tac_toe_gl/src/main.rs
+++ lib/tic_tac_toe_gl/src/main.rs
@@ -16,12 +16,14 @@ fn main() {
 
     implement_vertex!(Vertex, position);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [0.0, 0.5] };
-    let vertex3 = Vertex { position: [0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex1 = Vertex { position: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [1.0, 0.0] };
+    let vertex3 = Vertex { position: [1.0, 1.0] };
+    let vertex4 = Vertex { position: [0.0, 1.0] };
 
-    let shape_indices = vec![0u8, 1, 2];
+    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+
+    let shape_indices = vec![1u8, 0, 2, 0, 3 2];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::IndexBuffer::new(&display,
